8d228e8b3ff5e408fb96c6bc6473bb1ea175f078
shahabyounas <younasshahab@gmail.com>
2019-06-24 15:06:23 +0500

    Updates

---
+++ precache-manifest.eece95207bb9136b1aa74254f033d715.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a2cd831186ce62496aa182176f80a3",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "6c9e53388346e4b3dfed",
+    "url": "/react-tic-tac-toe/static/css/main.b4afb38c.chunk.css"
+  },
+  {
+    "revision": "d5b0434b86f8949608d7",
+    "url": "/react-tic-tac-toe/static/js/2.4424779a.chunk.js"
+  },
+  {
+    "revision": "6c9e53388346e4b3dfed",
+    "url": "/react-tic-tac-toe/static/js/main.53ca1246.chunk.js"
+  },
+  {
+    "revision": "140d55aca7c0751087c1",
+    "url": "/react-tic-tac-toe/static/js/runtime~main.a5f725ed.js"
+  }
+]);
